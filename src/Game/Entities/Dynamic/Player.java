de96ac6d269d87b243be1f3d1bdbc5b540c6c957
Josben <josben.cruz@upr.edu>
2019-09-11 15:54:04 -0400

    reduced code

--- src/Game/Entities/Dynamic/Player.java
+++ src/Game/Entities/Dynamic/Player.java
@@ -169,8 +169,10 @@ public class Player {
 
 	}
 	public void addTail(){
+		score += Math.sqrt(2*score+1);
 		lenght++;
 		Tail tail= null;
+		
 		switch (direction){
 		case "Left":
 			if( handler.getWorld().body.isEmpty()){
@@ -272,116 +274,16 @@ public class Player {
 		}
 		handler.getWorld().body.addLast(tail);
 		handler.getWorld().playerLocation[tail.x][tail.y] = true;
+		
+		
 
 	}
 
 	public void Eat(){
-		score += Math.sqrt(2*score+1);
-		lenght++;
-		Tail tail= null;
 		handler.getWorld().appleLocation[xCoord][yCoord]=false;
 		handler.getWorld().appleOnBoard=false;
-		switch (direction){
-		case "Left":
-			if( handler.getWorld().body.isEmpty()){
-				if(this.xCoord!=handler.getWorld().GridWidthHeightPixelCount-1){
-					tail = new Tail(this.xCoord+1,this.yCoord,handler);
-				}else{
-					if(this.yCoord!=0){
-						tail = new Tail(this.xCoord,this.yCoord-1,handler);
-					}else{
-						tail =new Tail(this.xCoord,this.yCoord+1,handler);
-					}
-				}
-			}else{
-				if(handler.getWorld().body.getLast().x!=handler.getWorld().GridWidthHeightPixelCount-1){
-					tail=new Tail(handler.getWorld().body.getLast().x+1,this.yCoord,handler);
-				}else{
-					if(handler.getWorld().body.getLast().y!=0){
-						tail=new Tail(handler.getWorld().body.getLast().x,this.yCoord-1,handler);
-					}else{
-						tail=new Tail(handler.getWorld().body.getLast().x,this.yCoord+1,handler);
-
-					}
-				}
-
-			}
-			break;
-		case "Right":
-			if( handler.getWorld().body.isEmpty()){
-				if(this.xCoord!=0){
-					tail=new Tail(this.xCoord-1,this.yCoord,handler);
-				}else{
-					if(this.yCoord!=0){
-						tail=new Tail(this.xCoord,this.yCoord-1,handler);
-					}else{
-						tail=new Tail(this.xCoord,this.yCoord+1,handler);
-					}
-				}
-			}else{
-				if(handler.getWorld().body.getLast().x!=0){
-					tail=(new Tail(handler.getWorld().body.getLast().x-1,this.yCoord,handler));
-				}else{
-					if(handler.getWorld().body.getLast().y!=0){
-						tail=(new Tail(handler.getWorld().body.getLast().x,this.yCoord-1,handler));
-					}else{
-						tail=(new Tail(handler.getWorld().body.getLast().x,this.yCoord+1,handler));
-					}
-				}
-
-			}
-			break;
-		case "Up":
-			if( handler.getWorld().body.isEmpty()){
-				if(this.yCoord!=handler.getWorld().GridWidthHeightPixelCount-1){
-					tail=(new Tail(this.xCoord,this.yCoord+1,handler));
-				}else{
-					if(this.xCoord!=0){
-						tail=(new Tail(this.xCoord-1,this.yCoord,handler));
-					}else{
-						tail=(new Tail(this.xCoord+1,this.yCoord,handler));
-					}
-				}
-			}else{
-				if(handler.getWorld().body.getLast().y!=handler.getWorld().GridWidthHeightPixelCount-1){
-					tail=(new Tail(handler.getWorld().body.getLast().x,this.yCoord+1,handler));
-				}else{
-					if(handler.getWorld().body.getLast().x!=0){
-						tail=(new Tail(handler.getWorld().body.getLast().x-1,this.yCoord,handler));
-					}else{
-						tail=(new Tail(handler.getWorld().body.getLast().x+1,this.yCoord,handler));
-					}
-				}
-
-			}
-			break;
-		case "Down":
-			if( handler.getWorld().body.isEmpty()){
-				if(this.yCoord!=0){
-					tail=(new Tail(this.xCoord,this.yCoord-1,handler));
-				}else{
-					if(this.xCoord!=0){
-						tail=(new Tail(this.xCoord-1,this.yCoord,handler));
-					}else{
-						tail=(new Tail(this.xCoord+1,this.yCoord,handler));
-					} System.out.println("Tu biscochito");
-				}
-			}else{
-				if(handler.getWorld().body.getLast().y!=0){
-					tail=(new Tail(handler.getWorld().body.getLast().x,this.yCoord-1,handler));
-				}else{
-					if(handler.getWorld().body.getLast().x!=0){
-						tail=(new Tail(handler.getWorld().body.getLast().x-1,this.yCoord,handler));
-					}else{
-						tail=(new Tail(handler.getWorld().body.getLast().x+1,this.yCoord,handler));
-					}
-				}
-
-			}
-			break;
-		}
-		handler.getWorld().body.addLast(tail);
-		handler.getWorld().playerLocation[tail.x][tail.y] = true;
+		addTail();
+		
 	}
 
 	public void kill(){
